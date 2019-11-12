dbbe1b09c77f3dcad1f9bf24cbfbaf8e5d82548f
priest1966 <53721478+priest1966@users.noreply.github.com>
2019-11-12 16:27:36 +0530

    Update auto_profile.py

--- stdplugins/auto_profile.py
+++ stdplugins/auto_profile.py
@@ -19,7 +19,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅 {DMY} | 3Cube | ⌚️ {HM}"
+        bio = f"📅 {DMY} | Priest | ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
