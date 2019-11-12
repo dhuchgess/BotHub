10bfdb369b0d704526a5c7fa21fa948e676ebeb3
priest1966 <53721478+priest1966@users.noreply.github.com>
2019-11-12 18:52:30 +0530

    Update promote.py

--- stdplugins/promote.py
+++ stdplugins/promote.py
@@ -85,4 +85,4 @@ async def _(event):
     except (Exception) as exc:
         await event.edit(str(exc))
     else:
-        await event.edit("admins has Successfully premoted Madanyu{}")
+        await event.edit("admins has Successfully premoted you")
