73c018925c5d0e763c86edac91c2401a30514f94
S A Sohan <50193747+shn999@users.noreply.github.com>
2019-11-15 19:19:40 +0600

    Update execmod.py

--- stdplugins/execmod.py
+++ stdplugins/execmod.py
@@ -1,4 +1,4 @@
-"""COMMAND : .cpu, .uptime, .suicide, .env, .pip, .neofetch, .date, .coffeehouse, .telegram, .stdplugins, .fast, .listpip, .fuggi"""
+"""COMMAND : .cpu, .uptime, .env, .pip, .neofetch, .coffeehouse, .telegram, .stdplugins, .fast, .listpip, .fuggi"""
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
