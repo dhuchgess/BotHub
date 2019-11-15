29028a7870febb08f2a6f1758f4bdd567e2df8a7
S A Sohan <50193747+shn999@users.noreply.github.com>
2019-11-15 20:41:05 +0600

    Update carbon.py

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -60,7 +60,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbon by [@Three_Cube_TeKnoways_Bot](https://www.github.com/mkaraniya/BotHub)",
+         caption="Carbon by [@sashn9]",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
