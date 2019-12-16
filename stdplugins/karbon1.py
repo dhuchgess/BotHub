a9d4864188ffb2b43754cef445d6b45210ee1ce0
Mobinul Islam Pranta <57030533+dhuchgess@users.noreply.github.com>
2019-12-17 03:39:10 +0600

    Update Karbon1.py

--- stdplugins/karbon1.py
+++ stdplugins/karbon1.py
@@ -60,7 +60,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Karbon1 by [@sashn9]",
+         caption="Karbon1 by [@dhuchges]",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
